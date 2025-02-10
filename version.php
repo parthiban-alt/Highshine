0820c51591c509f7d4ed57031ad1fab30f3d7f92
parthiban-alt <ps4972083@gmail.com>
2025-02-10 16:19:53 +0530

    add

---
+++ version.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="version.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="top-header">
+    <div class="row justify-content-between py-1">
+        <div class="col-xl-6 col-lg-6 col-md-12  my-1">
+            <div class="top-contact rounded-3  d-inline-flex justify-content-between">
+                <button class="text-decoration-none rounded-3 text-dark p-2 fs-4"style="border:none;font-weight:bold; ">
+                    Contact Us
+                </button>
+                <button style="background-color: #001844;border:none; " class="rounded-3 py-2 ps-2 pe-3 fs-4 text-white text-decoration-none ">
+                    <img src="assets/phone.png" class="img-fluid pe-1" style="width:30px;height:30px;" alt="phone_img" >
+                    <span class="float-end fw-bold">+91 98409 84513</span>
+                </button>
+
+            </div>
+        </div>
+
+        <div class="col-xl-4 col-lg-4 col-md-12  my-1 d-flex justify-content-sm-center justify-content-md-end">
+            <div class=" rounded-4  d-inline-flex  align-items-center top-contact-right">
+                <button class="yellow-button rounded-3  text-decoration-none "style=" border:none;padding:10px;border-radius:20px;font-size:20px;background-color:#FFC420;font-weight:bold;">
+                    <img src="assets/arrow.png" alt="phone_img" class="img-fluid" style="background-color:yellow;width:20px;height:auto;">
+                    <span class="float-end "> &nbsp;Schedule a Free Consultation</span>
+                </button>
+             </div>
+        </div>
+      
+    </div>
+    </div>
+ 
+</div>
+
+<nav class="navbar sticky-top navbar-expand-lg" id="navbar">
+    <div class="container">
+    <div class="logo-img"><img src="assets/logo-top.png" alt="Logo" id="logo-top"></div>
+        <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse head_style navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav  ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link text-white">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white">Industries </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white">IFS</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Other Expertise
+                    </a>
+                   
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white">Careers</a>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+<main>
+<section id="hero" class="hero" >
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 text-light d-flex flex-column justify-content-center">
+                <div class="content">
+                    <h3 class="content-1">Redefining</h3>
+                    <h3 class="content-1">Business Realities with</h3>
+                    <h3 class="content-1 text-warning">ERP Solutions</h3>
+                    <hr class="w-100">
+                    <p class="para">Change is accelerating, and there's no better time to embrace modern business practices than now!</p>
+                </div>
+
+                <div class="rounded-4 d-inline-flex align-items-center banner_btn">
+                    <button class="yellow-button rounded-3 text-decoration-none"
+                        style="border:none;padding:10px;border-radius:20px;font-size:20px;background-color:yellow;font-weight:bold;">
+                        <img src="assets/arrow.png" alt="phone_img" class="img-fluid"
+                            style="background-color:#FFC420;width:20px;height:auto;">
+                        <span class="float-end para"> &nbsp;Schedule a Free Consultation</span>
+                    </button>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 d-flex justify-content-center">
+                <img src="assets/man.png" alt="slider1" class="responsive-img" id="hero-man" style="width:500px;height:500px;margin-top:60px;">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="experience exp-bg" id="experience">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-6 col-md-5 col-sm-5">
+                    <div class="exp-content">
+                        <!-- <p style="font-size: 36px;">YOUR <span class="fw-bold"> BUSINESS </span> INEFFICIENCY IS <span class="fw-bold">OUR PROBLEM TO SOLVE!</span></p> -->
+                        <h6 class="content-1" style="color:#001844;">LET'S <span class="fw-bold">TRANSFORM</span> YOUR <span class="fw-bold"> BUSINESS </span> CHALLENGES <span class="fw-bold">INTO OPPORTUNITIES!</span></h6>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-7 col-sm-7 exp-content">
+                    <p class="para">We bring in more than a decade experience from different industries combining with the latest technologies in the market. We make sure, we adopt the best fit.</p>
+                </div>
+            </div>
+            </div>
+            </div>
+            <section class="experiance-1" id="experiance-1"  style="background: url('assets/experiance.png') no-repeat center center/cover; width:100%; height: auto;padding-top:70px;padding-bottom:10px;">
+                    <div class="row text-white justify-content-end ">
+                        <div class="col-md-6">
+                            <!-- <p style="font-size: 40px; line-height: 1; ">We Bring In The Industry And<br> Technology Experience. We Have<br> Partnered With <span class="yellow-font">BEST ERPs</span>.</p> -->
+                            <h6 class="content-1 mb-5 " id="exp-text">We leverage our industry expertiseTo deliver tailored software solutionsThat drive<span class="text-warning"> your growth.</span></h6>
+                        </div>
+                    </div>
+                    <div class="row text-white py-3 exp-count">
+                        <div class="col-md-4 col-sm-4 text-center border-end border-3">
+                            <div>
+                                <h1 class="text-warning content-1">40<sup>+</sup></h1>
+                            </div>
+                            <p class="para">Customer business transformations</p>
+                        </div>
+                        <div class="col-md-4 col-sm-4  text-center border-end border-3">
+                            <div>
+                                <h1 class="text-warning content-1" >35<sup>+</sup></h1>
+                            </div>
+                            <p class="para">Employees</p>
+                        </div>
+                        <div class="col-md-4 col-sm-4 text-center">
+                            <div>
+                                <h1 class="text-warning content-1">28<sup>+</sup></h1>
+                            </div>
+                            <p class="para">Million USD customer savings by process optimization </p>
+                        </div>
+            </section>
+
+    </section>
+    <section id="offering" class="offering ifs-odoo-bg">
+        <div class="row py-2">
+            <div class="section-title">
+                <h1 class="text-center text-uppercase">Our <span class="yellow-font">Portfolio</span></h1>
+            </div>
+        </div>
+        <div class="container my-3 ">
+            <!-- First Row -->
+            <div class="row  justify-content-between mb-100">
+                <div class="col-lg-6 col-md-6 col-sm-6 mb-sm-10">
+                    <div class="sub-sec-title">
+                        <h1><span style="color: #8427E2;">IFS </span> Services Partner</h1>
+                        <h5>- A partnership with Tech Mahindra </h5>
+                    </div>
+                    <div class="content">
+                        <p>IFS is a robust ERP solution known for its comprehensive capabilities in asset management, project-driven operations, and scalability. Its strength lies in providing deep industry-specific solutions tailored to complex business needs.</p>
+                    </div>
+
+                    <!-- <a href="https://www.techmahindra.com/" target="_blank" rel="noopener noreferrer" class="pt-4">IFS Silver partner<br> - A Tech Mahindra partnership</a> -->
+                    <div class="bg-light rounded-4 text-dark d-inline-flex justify-content-between">
+                        <a href="ifs_v2.php" class="rounded-3 text-dark text-decoration-none  learn-more ">
+                            <span class="float-end fw-bold">Learn More</span>
+                            <img src="assets/images/send_yellow_arrow.png" alt="phone_img" class="img-fluid me-2">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6  col-sm-6 d-flex flex-column  align-items-end">
+                    <a href="ifs_v2.php" class="" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/IFS.png" alt="IFS_logo_2021" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+
+            <!-- Second Row -->
+            <div class="row  justify-content-between mt-50">
+                <!-- Odoo Logo aligned to bottom of the left column -->
+                <!-- <div class="col-lg-6 d-flex flex-column justify-content-end"> -->
+                <div class="col-lg-6 col-md-6  col-sm-6 d-flex flex-column justify-content-center align-items-center mt-sm-10 order-2 order-md-1 ">
+                    <a href="odoo_v2.php" target="_blank" rel="noopener noreferrer">
+                        <img class="w-75 " src="assets/Odoo.png" alt="Odoo_logo">
+                    </a>
+                </div>
+
+                <!-- Paragraph aligned to bottom of the right column -->
+                <div class="col-lg-6 col-md-6  col-sm-6 mt-50 order-1 order-md-2">
+                    <div class=" d-flex flex-column  align-items-end ">
+
+                        <div class="content">
+                            <div class="sub-sec-title">
+                                <h1 class="align-items-start ">
+                                    <span style="color:#9C5789;">Odoo </span>
+                                    Official Partner
+                                </h1>
+                            </div>
+
+                            <p>We specialize in implementing Odoo, an ideal solution for small to mid-sized companies seeking a flexible and cost-effective ERP system. Odoo’s modular approach allows businesses to scale and customize functionalities as they grow.</p>
+                        </div>
+                        <div class="bg-light rounded-4 text-dark d-inline-flex justify-content-between ">
+                            <button class="rounded-3 text-dark text-decoration-none  learn-more">
+                                <span class="float-end fw-bold">Learn More</span>
+                                <img src="assets/phone.png" alt="phone_img" class="img-fluid me-2">
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer class="footer mt-4  py-4 footer">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-sm-12 ">
+                <img src="assets/footer-logo.png" class="img-fluid" alt="img">
+                <p class="para text-light">Redefining Business Realities with ERP Solutions</p>
+                <div class="bg-light text-dark d-inline-flex justify-content-between top-contact" style=" border-radius:10px;">
+                    <button class="fw-bold text-dark  para"style="border:none; border-radius:20px;padding:5px;">
+                        Contact Us
+                    </button>
+                    <button style="background-color: #FFB606;border:none; border-radius:10px;" class=" text-dark ">
+                        <img src="assets\black-phone.png" alt="phone_img" class="img-fluid me-1" style="border:none;padding:10px;">
+                        <span class="fw-bold text-dark  para">+91 98409 84513</span>
+                    </button>
+                </div><br>
+               
+            </div>
+            <div class="col-lg-4 col-sm-6  d-flex justify-content-lg-center mt-2">
+                <div class="f-title">
+                    <h3 class="txt text-warning">GLOBAL PRESENCE</h3>
+                    <a class="text-decoration-none text-white d-inline-flex" href="https://maps.app.goo.gl/7HAcdGvS4GQkxSWQ7" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/black-arrow.png" width="34" height="34" class="me-2 p-1 fw-bold " alt="send_arrow">
+                        <address><span class="text-warning fw-bold para">India</span> <br> No: 50, Eswaran Koil Street,<br> Zamin Pallavaram, Chennai 600117.</address>
+                    </a>
+                    <hr class="text-white">
+                    <a class="text-decoration-none text-white d-inline-flex" rel="noopener noreferrer">
+                        <img src="assets/black-arrow.png" width="34" height="34" class="me-2 p-1 " alt="send_arrow">
+                        <address><span class="text-warning fw-bold para">USA</span> <br>539 W. Commerce St #5263,<br> Dallas, Texas 75208, USA.</address>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 d-flex justify-content-lg-center mt-2">
+                <div class="f-title">
+
+                    <h3 class="ms-2 text-warning">MENU</h3>
+                    <ul>
+                        <li class="text-warning"><a class="text-decoration-none text-white" >Home</a></li>
+                        <li class="text-warning"><a class="text-decoration-none text-white">Industries</a></li>
+                        <li class="text-warning"><a class="text-decoration-none text-white">IFS</a></li>
+                        <li class="text-warning"><a class="text-decoration-none text-white">Odoo</a></li>
+                        <li class="text-warning"><a class="text-decoration-none text-white">About Us</a></li>
+                        <li class="text-warning"><a class="text-decoration-none text-white"">Careers</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="text-white">
+        <div class="row text-center text-white">
+            <div class="col-md-6">
+                <p class="d-inline-flex float-lg-start">  All rights reserved By Airavad Web Solutions</p>               
+                   
+            </div>
+            <div class="col-md-6">
+                <div class="d-inline-flex float-lg-end">
+                    <p class="me-2  text-white">Terms &amp; Conditions</p>
+                    <p class="me-2  text-white">Privacy Policy</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
